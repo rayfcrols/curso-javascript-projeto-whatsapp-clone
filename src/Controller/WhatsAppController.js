9239601b9d45167ae34f9d7ae6a7586757d50d51
Raimundo Luiz <rayfcrols@gmail.com>
2019-10-28 18:00:14 -0300

    Exibindo e Ocultando Paneis

--- src/Controller/WhatsAppController.js
+++ src/Controller/WhatsAppController.js
@@ -3,6 +3,7 @@ class WhatsAppController {
     console.log('WhatsAppController OK');
     this.elementsPrototype();
     this.loadElements();
+    this.initEvents();
   }
 
   loadElements() {
@@ -60,5 +61,40 @@ class WhatsAppController {
     Element.prototype.hasClass = function(name) {
       return this.classList.contains(name);
     };
+  }
+
+  initEvents() {
+    this.el.myPhoto.on('click', (e) => {
+      this.closeAllLeftPanel();
+      this.el.panelEditProfile.show();
+      setTimeout(() => {
+        this.el.panelEditProfile.addClass('open');
+      }, 300);
+    });
+
+    this.el.btnClosePanelEditProfile.on('click', (e) => {
+      this.el.panelEditProfile.removeClass('open');
+    });
+
+    this.el.btnNewContact.on('click', (e) => {
+      this.closeAllLeftPanel();
+      this.el.panelAddContact.show();
+      setTimeout(() => {
+        this.el.panelAddContact.addClass('open');
+      }, 300);
+    });
+
+    this.el.btnClosePanelAddContact.on('click', (e) => {
+      this.el.panelAddContact.removeClass('open');
+    });
+
+    this.el.photoContainerEditProfile.on('click', (e) => {
+      this.el.inputProfilePhoto.click();
+    });
+  }
+
+  closeAllLeftPanel() {
+    this.el.panelAddContact.hide();
+    this.el.panelEditProfile.hide();
   }
 }
