df2364a488fbc9915153ab5ced05f18271a2d66b
Raimundo Luiz Oliveira <rayfcrols@gmail.com>
2019-11-22 01:36:05 +0000

    Identificação dos tipos de arquivos no envio

--- src/Controller/DocumentPreviewController.js
+++ src/Controller/DocumentPreviewController.js
@@ -1,45 +1,48 @@
 export class DocumentPreviewController {
-  constructor(file) {
-    this._file = file;
-  }
+    constructor(file) {
+        this._file = file;
+    }
 
-  getPreviewData() {
-    return new Promise((s, f) => {
-      switch (this._file.type) {
-        case 'image/jpeg':
-        case 'image/jpg':
-        case 'image/bmp':
-        case 'image/png':
-        case 'image/gif':
-          let reader = new FileReader();
-          reader.onload = (e) => {
-            s(reader.result);
-          };
-          reader.onerror = (e) => {
-            f(e);
-          };
-          reader.readAsDataURL(this._file);
-          break;
-        case 'video/mp4':
-        case 'video/x-msvideo':
-        case 'video/mpeg':
-        case 'video/ogg':
-        case 'video/3gpp':
-          console.log('img');
-          break;
-        case 'application/pdf':
-        case 'application/x-pkcs12':
-          console.log('img');
-          break;
-        case 'audio/ogg':
-        case 'audio/mp3':
-        case 'audio/mpeg':
-        case 'audio/x-wav':
-          console.log('img');
-          break;
-        default:
-          f();
-      }
-    });
-  }
+    getPreviewData() {
+        return new Promise((s, f) => {
+            switch(this._file.type) {
+                case 'image/jpeg':
+                case 'image/jpg':
+                case 'image/bmp':
+                case 'image/png':
+                case 'image/gif':
+                    let reader = new FileReader();
+                    reader.onload = (e) => {
+                        s({
+                            src: reader.result,
+                            info: this._file.name
+                        });
+                    };
+                    reader.onerror = (e) => {
+                        f(e);
+                    };
+                    reader.readAsDataURL(this._file);
+                    break;
+                case 'video/mp4':
+                case 'video/x-msvideo':
+                case 'video/mpeg':
+                case 'video/ogg':
+                case 'video/3gpp':
+                    console.log('video');
+                    break;
+                case 'application/pdf':
+                case 'application/x-pkcs12':
+                    console.log('pdf');
+                    break;
+                case 'audio/ogg':
+                case 'audio/mp3':
+                case 'audio/mpeg':
+                case 'audio/x-wav':
+                    console.log('audio');
+                    break;
+                default:
+                    f();
+            }
+        });
+    }
 }
